3443ce1014016edcbde23861ffb87035498d8a61
2653918681@qq.com <austinchen>
2020-07-29 22:15:32 +0800

    no message

--- DjangoDemon/polls/models.py
+++ DjangoDemon/polls/models.py
@@ -9,13 +9,14 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    question_state = models.IntegerField(default=0)
+    # question_state = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question_text+" careted at "+self.pub_date
+        return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
